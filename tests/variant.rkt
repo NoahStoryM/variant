fd274209b7e118cc8c5a25a32db56fd386af09cb
NoahStoryM <NoahStoryM@gmail.com>
2025-06-19 17:11:06 +0800

    Add tests

---
+++ tests/variant.rkt
@@ -0,0 +1,85 @@
+#lang racket/base
+
+(require rackunit)
+(require "../main.rkt")
+
+(displayln "Test `variant.rkt'")
+
+(define-syntax-rule (check-variant= e1 e2)
+  (check-equal?
+   (let*-variant ([(#:tag [n 0] . v1) e1]) (cons v1 n))
+   (let*-variant ([(#:tag [n 0] . v2) e2]) (cons v2 n))))
+
+(test-case "Test `tag'"
+  (for ([n (in-range 10)])
+    (check-pred tag? (tag n)))
+  (check-exn exn:fail:contract? (λ () (tag -1)))
+  (check-exn exn:fail:contract? (λ () (tag "1"))))
+
+(test-case "Test `variant'"
+  (check-variant= (variant 1 2 3)         (values 1 2 3))
+  (check-variant= (variant 1 2 3 #:tag 0) (values 1 2 3))
+  (check-variant= (variant 1 2 3)         (values (tag 0) 1 2 3))
+  (check-variant= (variant 1 2 3 #:tag 0) (values (tag 0) 1 2 3))
+  (check-variant= (variant 1 2 3 #:tag 1) (values (tag 1) 1 2 3)))
+
+(test-case "Test `apply/variant'"
+  (check-eqv? (apply/variant + 1 2 (list 3)) 6)
+  (check-eqv? (apply/variant + 1 2 (list 3) #:tag 0) 6)
+  (check-exn exn:fail:contract?
+             (λ () (apply/variant + 1 2 (list 3) #:tag 1)))
+  (check-equal?
+   (apply/variant
+    (λ (a b #:tag [n 0]) (cons (vector a b) n))
+    (list 1 2) #:tag 1)
+   '(#(1 2) . 1)))
+
+(test-case "Test `call-with-variant'"
+  (check-equal? (call-with-variant (λ () (variant 'a 'b)) cons) '(a . b))
+  (check-equal? (call-with-variant (λ () (variant 'a 'b #:tag 0)) cons) '(a . b))
+  (check-exn exn:fail:contract?
+             (λ () (call-with-variant (λ () (variant 'a 'b #:tag 1)) cons))))
+
+(test-case "Test `let*-variant'"
+  (check-equal? (let*-variant ([v* (variant 1 2 3)]) v*) '(1 2 3))
+  (check-equal? (let*-variant ([(v . v*) (variant 1 2 3)]) (cons v* v))
+                (let*-variant ([(v . v*) (variant 1 2 3 #:tag 0)]) (cons v* v)))
+  (check-exn exn:fail:contract?
+             (λ () (let*-variant ([(v . v*) (variant 1 2 3 #:tag 1)]) (cons v* v))))
+  (check-equal? (let*-variant ([(#:tag n v . v*)
+                                (variant 1 2 3 #:tag 1)])
+                              (cons (cons v* v) n))
+                '(((2 3) . 1) . 1))
+  (check-equal? (let*-variant ([(#:tag [n 0] v . v*)
+                                (variant 1 2 3)])
+                              (cons (cons v* v) n))
+                '(((2 3) . 1) . 0))
+  (check-exn exn:fail:contract?
+             (λ () (let*-variant ([(#:tag n v . v*) (variant 1 2 3)]) (cons (cons v* v) n))))
+  (check-exn exn:fail:contract?
+             (λ () (let*-variant ([(#:tag n v . v*) (variant 1 2 3 #:tag 0)]) (cons (cons v* v) n)))))
+
+(test-case "Test `define-variant'"
+  (check-equal? (let () (define-variant v* (variant 1 2 3)) v*) '(1 2 3))
+  (check-equal? (let () (define-variant (v . v*) (variant 1 2 3)) (cons v* v))
+                (let () (define-variant (v . v*) (variant 1 2 3 #:tag 0)) (cons v* v)))
+  (check-exn exn:fail:contract?
+             (λ ()
+               (define-variant (v . v*) (variant 1 2 3 #:tag 1))
+               (cons v* v)))
+  (check-equal? (let ()
+                  (define-variant (#:tag n v . v*) (variant 1 2 3 #:tag 1))
+                  (cons (cons v* v) n))
+                '(((2 3) . 1) . 1))
+  (check-equal? (let ()
+                  (define-variant (#:tag [n 0] v . v*) (variant 1 2 3))
+                  (cons (cons v* v) n))
+                '(((2 3) . 1) . 0))
+  (check-exn exn:fail:contract?
+             (λ ()
+               (define-variant (#:tag n v . v*) (variant 1 2 3))
+               (cons (cons v* v) n)))
+  (check-exn exn:fail:contract?
+             (λ ()
+               (define-variant (#:tag n v . v*) (variant 1 2 3 #:tag 0))
+               (cons (cons v* v) n))))
